600929e9c5acfb3c5adb96fc4a8e81538bbbecc3
MisteriDelx <126623869+MisteriDelx@users.noreply.github.com>
2023-05-07 21:49:53 +0200

    Add files via upload

--- 4-Multitarea-manual.ino
+++ 4-Multitarea-manual.ino
@@ -1,77 +1,60 @@
-#define LED_1_PIN 9
-#define LED_2_PIN 10
-#define LED_3_PIN 11
-#define LED_4_PIN 12
+/*
+ * PRÁCTICA PARA PROBAR LA MULTITAREA EN ARDUINO Y LA FUNCIÓN millis()
+ */
+// Se declaran las variables con "define" en lugar de con int o long para poder usar nombres para cada variable
+#define LED1_PIN 12 ; // creamos 3 variables y les adjudicamos el valor que representa el 
+                      //número de pin al que va conectado cada LED
+#define LED2_PIN 11 ;
+#define LED3_PIN 10 ;
 
-#define POTENTIOMETER_PIN A0
+#define PULSADOR_PIN A1 // el pulsador irá conectado al pin analógico A1
 
-#define BUTTON_PIN 5
+unsigned long tiempo_anterior_LED1 = millis();
+long intervalo_tiempo_LED1 = 1000; // el LED1 parpadeará cada 1 segundo
+int estado_LED1 = LOW; // el LED1 comenzará en estado bajo o apagado
 
-unsigned long previousTimeLed1 = millis();
-long timeIntervalLed1 = 1000;
-int ledState1 = LOW;
-
-unsigned long previousTimeSerialPrintPotentiometer = millis();
-long timeIntervalSerialPrint = 2000;
+unsigned long tiempo_anterior_Potencimetro= millis();
+long intervalo_tiempoPotenciometro = 2000;
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  
-  pinMode(LED_1_PIN, OUTPUT);
-  pinMode(LED_2_PIN, OUTPUT);
-  pinMode(LED_3_PIN, OUTPUT);
-  pinMode(LED_4_PIN, OUTPUT);
-  pinMode(BUTTON_PIN, INPUT);
+    // El código aquí representado sólo se ejecutará una vez
+    Serial.begin(9600); //inicializamos la comunicación serie
+    
+    pinMode(LED1_PIN, OUTPUT);
+    pinMode(LED2_PIN, OUTPUT);
+    pinMode(LED3_PIN, OUTPUT);
+  // no es necesario configurar la entrada analógica porque está configurada como entrada por defecto
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  unsigned long currentTime = millis();
-
-  // task 1
-  if(currentTime - previousTimeLed1 > timeIntervalLed1) {
-    previousTimeLed1 = currentTime;
-
-    if (ledState1 == HIGH) {
-      ledState1 = LOW;
+  unsigned long actualTime = millis();
+  
+  // TAREA 1. Conmutar el estado del LED1 cada cierto intervalo de tiempo
+  if(actualTime - tiempo_anterior_LED1 > intervalo_tiempo_LED1) {
+  tiempo_anterior_LED1 = actualTime; // tiempo_anterior_LED1 para a tomar el valor de actualTime
+      if (estadoLED1 == HIGH) {
+        estadoLED1 = LOW; //cambiamos el estado del LED1
+      }
+      else {
+        estadoLED1 = HIGH;
+      }
+  
+      digitalWrite(LED1_PIN, estadoLED1);
+    }
+  
+  // TAREA2. Variamos la luminosidad del LED2 desde la entrada del monitor serie
+  if (Serial.available()) {
+      int userInput = Serial.parseInt(); //declaramos una variable de tipo entero llamada userInput que tomará el valor del entero que le llegue por el puerto serie
+      if (userInput >= 0 && userInput < 256) { //si el valor se encuentra entre 0 y 255 lo que equivale a 0V o 5 V  entonces...
+        analogWrite(LED2_PIN, userInput); // mostramos el valor leído desde el monitor serie en el LED2
+      }
+    }
+  
+    // TAREA3. Encender el LED3 desde un botón
+    if (analogRead(PULSADOR_PIN) >150) {
+       digitalWrite(LED3_PIN, HIGH);
     }
     else {
-      ledState1 = HIGH;
+      digitalWrite(LED3_PIN, LOW);
     }
-
-    digitalWrite(LED_1_PIN, ledState1);
-  }
-
-  // task 2
-  if (Serial.available()) {
-    int userInput = Serial.parseInt();
-    if (userInput >= 0 && userInput < 256) {
-      analogWrite(LED_2_PIN, userInput);
-    }
-  }
-
-  // task 3
-  if (digitalRead(BUTTON_PIN) == HIGH) {
-     digitalWrite(LED_3_PIN, HIGH);
-  }
-  else {
-    digitalWrite(LED_3_PIN, LOW);
-  }
-
-  // task 4
-  int potentiometerValue = analogRead(POTENTIOMETER_PIN);
-  if (potentiometerValue > 512) {
-    digitalWrite(LED_4_PIN, HIGH);
-  }
-  else {
-    digitalWrite(LED_4_PIN, LOW);
-  }
-
-  // task 5
-  if (currentTime - previousTimeSerialPrintPotentiometer > timeIntervalSerialPrint) {
-    previousTimeSerialPrintPotentiometer = currentTime;
-    Serial.print("Value : ");
-    Serial.println(potentiometerValue);
-  }
 }
