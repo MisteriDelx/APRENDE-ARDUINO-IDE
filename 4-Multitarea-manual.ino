d9b9cbb246de1ae9fd22c286859404cee1fd564d
MisteriDelx <126623869+MisteriDelx@users.noreply.github.com>
2023-05-07 23:06:24 +0200

    Add files via upload

--- 4-Multitarea-manual.ino
+++ 4-Multitarea-manual.ino
@@ -2,16 +2,15 @@
  * PRÁCTICA PARA PROBAR LA MULTITAREA EN ARDUINO Y LA FUNCIÓN millis()
  */
 // Se declaran las variables con "define" en lugar de con int o long para poder usar nombres para cada variable
-#define LED1_PIN 12 ; // creamos 3 variables y les adjudicamos el valor que representa el 
+int LED1=12 ; // creamos 3 variables y les adjudicamos el valor que representa el 
                       //número de pin al que va conectado cada LED
-#define LED2_PIN 11 ;
-#define LED3_PIN 10 ;
-
-#define PULSADOR_PIN A1 // el pulsador irá conectado al pin analógico A1
+int LED2=11;
+int LED3=10;
+int PULSADOR=A1; // el pulsador irá conectado al pin analógico A1 aunque podríamos utilizar un pin digital
 
 unsigned long tiempo_anterior_LED1 = millis();
 long intervalo_tiempo_LED1 = 1000; // el LED1 parpadeará cada 1 segundo
-int estado_LED1 = LOW; // el LED1 comenzará en estado bajo o apagado
+int estadoLED1 = LOW;            // el LED1 comenzará en estado bajo o apagado
 
 unsigned long tiempo_anterior_Potencimetro= millis();
 long intervalo_tiempoPotenciometro = 2000;
@@ -20,10 +19,10 @@ void setup() {
     // El código aquí representado sólo se ejecutará una vez
     Serial.begin(9600); //inicializamos la comunicación serie
     
-    pinMode(LED1_PIN, OUTPUT);
-    pinMode(LED2_PIN, OUTPUT);
-    pinMode(LED3_PIN, OUTPUT);
-  // no es necesario configurar la entrada analógica porque está configurada como entrada por defecto
+    pinMode(LED1, OUTPUT);
+    pinMode(LED2, OUTPUT);
+    pinMode(LED3, OUTPUT);
+  // No es necesario configurar la entrada analógica porque está configurada como entrada por defecto
 }
 
 void loop() {
@@ -38,23 +37,22 @@ void loop() {
       else {
         estadoLED1 = HIGH;
       }
-  
-      digitalWrite(LED1_PIN, estadoLED1);
+      digitalWrite(LED1,estadoLED1);
     }
   
   // TAREA2. Variamos la luminosidad del LED2 desde la entrada del monitor serie
   if (Serial.available()) {
       int userInput = Serial.parseInt(); //declaramos una variable de tipo entero llamada userInput que tomará el valor del entero que le llegue por el puerto serie
       if (userInput >= 0 && userInput < 256) { //si el valor se encuentra entre 0 y 255 lo que equivale a 0V o 5 V  entonces...
-        analogWrite(LED2_PIN, userInput); // mostramos el valor leído desde el monitor serie en el LED2
+        analogWrite(LED2, userInput); // mostramos el valor leído desde el monitor serie en el LED2
       }
     }
   
-    // TAREA3. Encender el LED3 desde un botón
-    if (analogRead(PULSADOR_PIN) >150) {
-       digitalWrite(LED3_PIN, HIGH);
+    // TAREA3. Encender el LED3 desde un pulsador
+    if (analogRead(PULSADOR) >150) {
+       digitalWrite(LED3, HIGH);
     }
     else {
-      digitalWrite(LED3_PIN, LOW);
+      digitalWrite(LED3, LOW);
     }
 }
